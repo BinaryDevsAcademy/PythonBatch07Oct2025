d65dade040e69625078916eff128ae2e4152537e
Dinesh Sripathi <dineshsri718@gmail.com>
2025-10-07 16:40:39 +0530

    First session completed....

---
+++ CorePython/Intro.py
@@ -0,0 +1,24 @@
+""" 
+    What is python
+    History of Python
+    Software Requirements of python
+    Indentation
+    Data Types
+    Print()
+    type()
+    String - intro
+"""
+
+print('That\'s a point...')
+print("That's a point...")
+print('"How are you?", she asked him')
+print('''print('That\'s a point...')
+      print("That's a point...")
+      print('"How are you?", she asked him') ''')
+print(True)
+
+# type() is used to check the type of the value
+print(type(1))
+print(type(1.4))
+print(type("1"))
+print(type(True))
